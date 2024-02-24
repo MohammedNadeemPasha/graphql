b01cff4f81c9a034ff2ac29bc18549359473b247
Testing <Testing@Nadeems-Air.lan>
2024-02-23 23:30:06 -0500

    added mutations

--- products/products.model.js
+++ products/products.model.js
@@ -2,18 +2,55 @@ const products = [
     {
         id:'redshoe',
         description:' Red Shoe',
-        price: 42.12
+        price: 42.12,
+        reviews:[]
     },
     {
         id:'bluejean',
         description:'Blue Jeans',
-        price: 45.16
+        price: 45.16,
+        reviews:[]
     }
 ]
 function getAllProducts(){
     return products;
 }
 
+function getProductsByPrice(min,max){
+    return products.filter((product) =>{
+        return product.price >= min && product.price <= max
+    })
+}
+
+function addNewProduct(id, description, price){
+    const newProduct = {
+        id,
+        description,
+        price,
+        reviews:[]
+    }
+
+    products.push(newProduct);
+    return newProduct;
+}
+
+function addNewProductReview(id,rating,comment){
+    const selectedProduct = products.find((product) =>{
+        return product.id === id
+    })
+    if(selectedProduct){
+        const newReview = {
+            rating,
+            comment
+        }
+        selectedProduct.reviews.push(newReview)
+        return newReview
+    }
+}
+
 module.exports = {
     getAllProducts,
+    getProductsByPrice,
+    addNewProduct,
+    addNewProductReview
 }
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -1,8 +1,19 @@
 const productsModel = require('./products.model')
 module.exports = {
     Query:{
-        products: (parent) => {
+        products: () => {
             return productsModel.getAllProducts();
+        },
+        productsByPrice: (_,args) =>{
+            return productsModel.getProductsByPrice(args.min,args.max);
+        }
+    },
+    Mutation:{
+        addNewProduct: (_,args) =>{
+            return productsModel.addNewProduct(args.id,args.description,args.price)
+        },
+        addNewProductReview: (_,args) => {
+            return productsModel.addNewProductReview(args.id, args.rating,args.comment)
         }
     }
 }
