e7ed12c9a36c76c2a2a428cf23c553f1c68a81fc
Testing <Testing@Nadeems-Air.lan>
2024-02-28 16:40:53 -0500

    testing apollo server

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const path = require('path');
-const {buildSchema} = require('graphql');
-const {createYoga} = require('graphql-yoga');
+const cors = require('cors')
+const {ApolloServer} = require('@apollo/server')
+const {expressMiddleware} = require('@apollo/server/express4')
+// const {buildSchema} = require('graphql');
+// const {createYoga} = require('graphql-yoga');
 const {loadFilesSync} = require('@graphql-tools/load-files')
 const {makeExecutableSchema} = require('@graphql-tools/schema')
 
-const schemaText = `
-type Query{
-    products: [Product]
-    order: [Order]
-}
-`;
+// const schemaText = `
+// type Query{
+//     products: [Product]
+//     order: [Order]
+// }
+// `;
 
 const typesArray = loadFilesSync(path.join(__dirname,'**/*.graphql'))
 const resolverArray = loadFilesSync(path.join(__dirname,'**/*.resolvers.js'))
+
+async function startApolloServer(){
+    const app = express();
+
+    const schema = makeExecutableSchema({
+        typeDefs:typesArray,
+        resolvers:resolverArray,
+    })
+
+    const server = new ApolloServer({
+        schema
+    })
+
+    await server.start()
+
+    app.use(cors());
+    app.use(express.json());
+    app.use('/graphql', expressMiddleware(server));
+    app.listen(3001, () =>{
+        console.log('Running graphql server')
+    });
+}
 
 // const typesArray = loadFilesSync('**/*',{
 //     extentions: ['graphql'],
@@ -22,10 +47,6 @@ const resolverArray = loadFilesSync(path.join(__dirname,'**/*.resolvers.js'))
 //     extensions: ['resolvers.js'],
 // })
 
-const schema = makeExecutableSchema({
-    typeDefs:typesArray,
-    resolvers:resolverArray,
-})
 
 // const schema = buildSchema(`
 //     type Query{
@@ -53,13 +74,21 @@ const schema = makeExecutableSchema({
 //     }
 // `);
 
-const app = express();
+// const app = express();
 
-app.use('/graphql',createYoga({
-    schema:schema,
-    graphiql:true,
-}));
+// app.use('/graphql',createYoga({
+//     schema:schema,
+//     graphiql:true,
+// }));
 
-app.listen(3000, () =>{
-    console.log('Running graphql server')
-});
+// app.use('/graphql', createYoga({
+//     schema: schema,
+// }));
+
+// app.use('/graphql', () =>{});
+
+// app.listen(3000, () =>{
+//     console.log('Running graphql server')
+// });
+
+startApolloServer();
